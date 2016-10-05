d887c03e5d8496fa63ec9eb69d4f3731ee5b2df7
Patrice Lebel <patleb@users.noreply.github.com>
2016-10-05 19:32:10 -0400

    rspec added to template and bumped version to 0.1.0

--- lib/pluginizer/plugin_builder.rb
+++ lib/pluginizer/plugin_builder.rb
@@ -21,11 +21,23 @@ module Pluginizer
 
       after_bundle do
         in_root do
+          configure_rspec
+
           git :init
           git add: '.'
           git commit: "-m 'first commit'"
         end
       end
+    end
+
+    private
+
+    def configure_rspec
+      invoke('rspec:install')
+
+      gsub_file 'spec/rails_helper.rb',
+        "require File.expand_path('../../config/environment', __FILE__)",
+        "require File.expand_path('../dummy/config/environment', __FILE__)"
     end
   end
 end
