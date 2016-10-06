107c861a2d33d691fc22bd13883af37321f229dd
Patrice Lebel <patleb@users.noreply.github.com>
2016-10-06 15:05:16 -0400

    fantaskspec gem added and configured

--- lib/pluginizer/plugin_builder.rb
+++ lib/pluginizer/plugin_builder.rb
@@ -40,6 +40,14 @@ module Pluginizer
         "require File.expand_path('../dummy/config/environment', __FILE__)"
 
       run "bundle binstubs rspec-core"
+
+      { "# Add additional requires below this line. Rails is not loaded until this point!" =>
+          "\nrequire 'fantaskspec'\n",
+        %{# config.filter_gems_from_backtrace("gem name")} =>
+          "\n\n  config.infer_rake_task_specs_from_file_location!"
+      }.each do |after_line, new_line|
+        insert_into_file 'spec/rails_helper.rb', new_line, after: after_line
+      end
     end
   end
 end
