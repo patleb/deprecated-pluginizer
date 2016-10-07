3556eb6a4f663ec1821f785aa8a848049c5ce6c0
Patrice Lebel <patleb@users.noreply.github.com>
2016-10-07 14:34:41 -0400

    rspec configurations updated and version bumped to 0.1.4

--- lib/pluginizer.rb
+++ lib/pluginizer.rb
@@ -1,6 +1,7 @@
 require "pluginizer/version"
 require "pluginizer/generators/plugin_generator"
 require "pluginizer/plugin_builder"
+require 'rails/all'
 require 'active_support/all'
 
 module Pluginizer
--- lib/pluginizer/plugin_builder.rb
+++ lib/pluginizer/plugin_builder.rb
@@ -34,15 +34,26 @@ module Pluginizer
 
     def configure_rspec
       invoke('rspec:install')
+      rails_helper = 'spec/rails_helper.rb'
 
-      gsub_file 'spec/rails_helper.rb',
+      gsub_file rails_helper,
         "require File.expand_path('../../config/environment', __FILE__)",
         "require File.expand_path('../dummy/config/environment', __FILE__)"
+      gsub_file rails_helper,
+        %{config.fixture_path = "\#{::Rails.root}/spec/fixtures"},
+        %{config.fixture_path = "\#{#{camelized}::Engine.root}/spec/fixtures"}
 
       run "bundle binstubs rspec-core"
 
-      insert_into_file 'spec/rails_helper.rb', "\nrequire 'fantaskspec'\n", after: "# Add additional requires below this line. Rails is not loaded until this point!"
-      insert_into_file 'spec/rails_helper.rb', "\n  config.infer_rake_task_specs_from_file_location!\n", before: /^end/
+      insert_into_file rails_helper,
+        "\nrequire 'fantaskspec'\n",
+        after: "# Add additional requires below this line. Rails is not loaded until this point!"
+      insert_into_file rails_helper,
+        "\n  config.infer_rake_task_specs_from_file_location!\n",
+        before: /^end/
+      insert_into_file rails_helper,
+        "\n  config.render_views\n",
+        before: /^end/
       shoulda = <<-SHOULDA.strip_heredoc.indent(2)
 
         Shoulda::Matchers.configure do |config|
@@ -52,7 +63,10 @@ module Pluginizer
           end
         end
       SHOULDA
-      insert_into_file 'spec/rails_helper.rb', shoulda, before: /^end/
+      insert_into_file rails_helper, shoulda, before: /^end/
+      insert_into_file rails_helper,
+        "\n  config.include(Shoulda::Callback::Matchers::ActiveModel)\n",
+        before: /^end/
     end
   end
 end
